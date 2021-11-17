59efa819e1a3e8a4d412e59d86bb66dccbe16d46
igordavidovic <93661596+igordavidovic@users.noreply.github.com>
2021-11-17 20:46:18 +0100

    kodna stranica i insert

--- githubucenje.sql
+++ githubucenje.sql
@@ -1,8 +1,10 @@
 drop database if exists githubucenje;
-create database githubucenje;
+create database githubucenje character set utf8;
 use githubucenje;
 
 create table git(
     sifra int not null primary key auto_increment,
     naziv varchar(50)
 );
+
+insert into git(sifra,naziv) values(null,'Test');
