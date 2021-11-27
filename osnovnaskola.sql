174662a217f04522e84eb35854d6b83041637fd4
igordavidovic <93661596+igordavidovic@users.noreply.github.com>
2021-11-27 16:13:19 +0100

    Dodavanje podataka za osobu

--- osnovnaskola.sql
+++ osnovnaskola.sql
@@ -44,3 +44,25 @@ alter table uciteljica add foreign key(osoba) references osoba(sifra);
 
 alter table dijete add foreign key(osoba) references osoba(sifra);
 
+insert into osoba(sifra,ime,prezime,datumrodenja) values
+(null,'Vanja','Kokot',null),
+(null,'Anita','Crnčević',null),
+(null,'Ena','Živković',null),
+(null,'Vid','Vlahović',null),
+(null,'Krešimir','Vlahović',null),
+(null,'Veronika','Janković',"2010-10-11"),
+(null,'Daria','Janković',"2011-10-12"),
+(null,'Terezija','Božić',"2010-09-23"),
+(null,'Hana','Lončar',"2011-05-24"),
+(null,'Zvonimir','Martinović',null),
+(null,'Andrija','Filipović',null),
+(null,'Sunčica','Pavičić',null),
+(null,'Mario','Horvat',null),
+(null,'Darijo','Horvat',null),
+(null,'Dejan','Franić',null),
+(null,'Jasmina','Živković',null),
+(null,'Roko','Pavletić',"2011-09-14"),
+(null,'Milana','Novak',"2010-04-20"),
+(null,'Marin','Tadić',"2010-03-21"),
+(null,'Marko','Marković',"1989-03-24"),
+(null,'Marko','Horvat',null);
