357cf90743af526e53a58412111571999800d0f4
igordavidovic <93661596+igordavidovic@users.noreply.github.com>
2021-12-02 17:48:42 +0100

    Rješavanje zadataka

--- kvjezba/kolokvijvjezba3.sql
+++ kvjezba/kolokvijvjezba3.sql
@@ -81,3 +81,47 @@ alter table snasa add foreign key(ostavljena) references ostavljena(sifra);
 alter table ostavljena add foreign key(prijatelj) references prijatelj(sifra);
 
 alter table svekar add foreign key(cura) references cura(sifra);
+
+insert into prijatelj(sifra,kuna,haljina,lipa,dukserica,indiferentno) values
+(null,100.00,null,null,'Plava dukserica',1),
+(null,200.00,null,null,null,0),
+(null,200.00,'Crna haljina',null,null,1);
+
+insert into brat(sifra,jmbag,ogrlica,ekstroventno) values
+(null,null,8,0),
+(null,'56785678999',2,1),
+(null,null,3,0);
+
+insert into prijatelj_brat(sifra,prijatelj,brat) values
+(null,1,1),
+(null,2,2),
+(null,3,3);
+
+insert into ostavljena(sifra,kuna,lipa,majica,modelnaocala,prijatelj) values
+(null,null,null,null,'Aviator',1),
+(null,200.00,null,'Crna majica','Aviator',2),
+(null,100.00,null,'Crvena majica','Rayban',null);
+
+insert into snasa(sifra,kuna,eura,ostavljena) values
+(null,200.00,35.00,1),
+(null,100.00,20.00,2),
+(null,1500.00,300.00,3);
+
+update svekar set suknja='Osijek';
+
+delete from punica where kratkamajica='AB';
+
+select majica from ostavljena where lipa not in(9,10,20,30,35);
+
+select a.ekstroventno,f.vesta,e.kuna
+from brat a inner join prijatelj_brat b on a.sifra=b.brat 
+inner join prijatelj c on b.prijatelj=c.sifra 
+inner join ostavljena d on c.sifra=d.prijatelj 
+inner join snasa e on d.sifra=e.ostavljena 
+inner join punica f on e.sifra=f.snasa
+where d.lipa!=91 and c.haljina like '%ba%'
+order by 3 desc;
+
+select a.haljina,a.lipa
+from prijatelj a inner join prijatelj_brat b on a.sifra=b.prijatelj 
+where b.prijatelj is null;
