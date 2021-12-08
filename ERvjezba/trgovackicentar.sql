2fbf5cf1534f5c3ba4d948fb81f237433137e9d6
igordavidovic <93661596+igordavidovic@users.noreply.github.com>
2021-12-08 17:41:57 +0100

    Dodavanje inserta

--- ERvjezba/trgovackicentar.sql
+++ ERvjezba/trgovackicentar.sql
@@ -33,3 +33,35 @@ alter table sef add foreign key(osoba) references osoba(sifra);
 
 alter table trgovina_osoba add foreign key(trgovina) references trgovina(sifra);
 alter table trgovina_osoba add foreign key(osoba) references osoba(sifra);
+
+insert into osoba(sifra,ime,prezime,placa) values
+(null,'Igor','Davidović',null),
+(null,'Mateo','David',null),
+(null,'Matej','Točak',9000.00),
+(null,'Hrvoje','Tomasović',null),
+(null,'Tomislav','Lagani',null),
+(null,'Zdenka','Nazar',5600.00),
+(null,'Zvonimir','Varenica',null),
+(null,'Zdenko','Tomas',null),
+(null,'Ivan','Horvatinović',5000.00),
+(null,'Ivica','Horvat',4900.00);
+
+insert into sef(sifra,osoba) values
+(null,1),
+(null,2),
+(null,3),
+(null,4);
+
+insert into trgovina(sifra,naziv,vrsta,radnovrijeme,sef) values
+(null,'Oranica','Poljoprivredna trgovina','07-15',1),
+(null,'Boso','Minimarket','07-23',2),
+(null,'Ravlić','Mesnica','08-17',3),
+(null,'Tokić',null,'07-15',4);
+
+insert into trgovina_osoba(trgovina,osoba) values
+(1,5),
+(2,6),
+(2,7),
+(2,8),
+(3,9),
+(4,10);
