aeda2765a94814fd579eaa64722d97423f287e84
igordavidovic <93661596+igordavidovic@users.noreply.github.com>
2021-12-08 16:51:39 +0100

    Dodavanje tabela

---
+++ ERvjezba/srednjaskola.sql
@@ -0,0 +1,30 @@
+drop database if exists srednjaskola;
+create database srednjaskola character set utf8;
+use srednjaskola;
+
+create table razred(
+    sifra int not null primary key auto_increment,
+    naziv varchar(50) not null
+);
+
+create table ucenik(
+    sifra int not null primary key auto_increment,
+    ime varchar(50) not null,
+    prezime varchar(50) not null,
+    datumrodenja date,
+    razred int not null
+);
+
+create table profesor(
+    sifra int not null primary key auto_increment,
+    ime varchar(50) not null,
+    prezime varchar(50) not null,
+    email varchar(50),
+    placa decimal(18,2)
+);
+
+create table profesor_razred(
+    profesor int not null,
+    razred int not null
+);
+
