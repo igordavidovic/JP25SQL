ecce2a2caa37b700768a2945b9e0dc683b382d62
igordavidovic <93661596+igordavidovic@users.noreply.github.com>
2021-12-12 15:17:08 +0100

    Dodavanje podataka

--- ERvjezba/vodoinstalater.sql
+++ ERvjezba/vodoinstalater.sql
@@ -18,7 +18,7 @@ create table kvar(
 create table vodovodna_instalacija(
     sifra int not null primary key auto_increment,
     naziv varchar(50),
-    kvar varchar(50)
+    kvar int not null
 );
 
 create table segrt(
@@ -39,3 +39,26 @@ alter table popravak_kvar add foreign key(kvar) references kvar(sifra);
 alter table vodovodna_instalacija add foreign key(kvar) references kvar(sifra);
 
 alter table popravak add foreign key(segrt) references segrt(sifra);
+
+insert into segrt(sifra,ime,prezime,placa) values
+(null,'Igor','Davidović',5000.00);
+
+insert into kvar(sifra,naziv) values
+(null,'Pokvarena centrifuga'),
+(null,'Pokvarena mehanika'),
+(null,'Pokvarena mašina');
+
+insert into vodovodna_instalacija(sifra,naziv,kvar) values
+(null,'Vodovna instalacija 1',1),
+(null,'Vodovodna instalcaija 2',2),
+(null,null,3);
+
+insert into popravak(sifra,naziv,cijena,trajanje,segrt) values
+(null,'Popravak centrifuge',null,null,null),
+(null,'Zamjena mehanike',650.00,null,1),
+(null,'Zamjena mašine',450.00,null,1);
+
+insert into popravak_kvar(popravak,kvar) values
+(1,1),
+(2,2),
+(3,3);
