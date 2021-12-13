c59b8d00a32006c69cb011b53c5fd36225844147
igordavidovic <93661596+igordavidovic@users.noreply.github.com>
2021-12-13 13:05:32 +0100

    Ispravljanje greške, dodavanje ERA dijagrama

--- ERvjezba/odvjetnik.sql
+++ ERvjezba/odvjetnik.sql
@@ -11,7 +11,7 @@ create table odvjetnik(
 create table klijent(
     sifra int not null primary key auto_increment,
     ime varchar(50) not null,
-    prezime varchar(50) not null
+    prezime varchar(50) not null,
     oib char(11)
 );
 
