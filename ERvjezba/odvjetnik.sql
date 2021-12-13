5a4df21b57de4675d8db948d1b0bdd4e4f2c21f5
igordavidovic <93661596+igordavidovic@users.noreply.github.com>
2021-12-13 13:01:04 +0100

    Dodavanje atributa

--- ERvjezba/odvjetnik.sql
+++ ERvjezba/odvjetnik.sql
@@ -4,18 +4,27 @@ use odvjetnik;
 
 create table odvjetnik(
     sifra int not null primary key auto_increment,
+    ime varchar(50) not null,
+    prezime varchar(50) not null
 );
 
 create table klijent(
     sifra int not null primary key auto_increment,
+    ime varchar(50) not null,
+    prezime varchar(50) not null
+    oib char(11)
 );
 
 create table obrana(
     sifra int not null primary key auto_increment,
+    odvjetnik int not null,
+    klijent int not null
 );
 
 create table suradnik(
     sifra int not null primary key auto_increment,
+    ime varchar(50) not null,
+    prezime varchar(50) not null
 );
 
 create table odvjetnik_klijent(
